dce42274a03908e1c58acb452dac01eb6ae9c975
Warren MacEvoy <wmacevoy@gmail.com>
2022-01-31 13:14:19 -0700

    cpp and _if tests

---
+++ src/testfacts_if.c
@@ -0,0 +1,34 @@
+#include "facts.h"
+
+FACTS(Alpha) {
+  FACT_PRINT('c',<=,'b',"%c");
+}
+
+FACTS(Integer) {
+  int a=1,b=2;
+  FACT(a,==,b);
+}
+
+FACTS(Double) {
+  double a=1.3, b=2.4;
+  FACT(a,==,b);
+}
+
+FACTS(Err) {
+  fprintf(stderr,"message to stderr.\n");
+}
+
+FACTS(LargeErr) {
+  for (int i=0; i<100; ++i) {
+    fprintf(stderr,"message #%d to stderr.\n",i);
+  }
+}
+
+FACTS(Out) {
+  fprintf(stdout,"message to stderr.\n");
+}
+
+FACTS_FAST_IF(--facts) {
+  printf("main\n");
+  return 0;
+}
---
+++ src/testfacts_if.cpp
@@ -0,0 +1,34 @@
+#include "facts.h"
+
+FACTS(Alpha) {
+  FACT('c',<=,'b');
+}
+
+FACTS(Integer) {
+  int a=1,b=2;
+  FACT(a,==,b);
+}
+
+FACTS(Double) {
+  double a=1.3, b=2.4;
+  FACT(a,==,b);
+}
+
+FACTS(Err) {
+  std::cerr << "message to cerr." << std::endl;
+}
+
+FACTS(LargeErr) {
+  for (int i=0; i<100; ++i) {
+    std::cerr << "message #" << i << " to cerr." << std::endl;
+  }
+}
+
+FACTS(Out) {
+  std::cout << "message to cout." << std::endl;
+}
+
+FACTS_FAST_IF(--facts) {
+  std::cout << "main" << std::endl;
+  return 0;
+}
